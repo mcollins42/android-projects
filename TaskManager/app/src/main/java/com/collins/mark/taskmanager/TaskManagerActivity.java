db11c9fa24f0886ea0eca3ce856164cb17a546df
Mark Collins <mcollins42@users.noreply.github.com>
2017-03-04 12:06:53 -0700

    Empty Project for Task Manager

---
+++ TaskManager/app/src/main/java/com/collins/mark/taskmanager/TaskManagerActivity.java
@@ -0,0 +1,13 @@
+package com.collins.mark.taskmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_manager);
+    }
+}
